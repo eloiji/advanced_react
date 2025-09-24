b636e7a80015b5030ad8c2c44f3f564b3f8e0af2
eloiji <eloisajocsoniting@gmail.com>
2025-09-25 00:27:09 +0800

    Added Exercise for React Context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import RegistrationForm from "./components/RegistrationForm";
+import Root from "./components/ThemeSettings";
 
 function App() {
   return (
     <div className="App">
       <RegistrationForm />
+      <Root />
     </div>
   );
 }
