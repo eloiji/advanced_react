caa4c22e6de96e796373ee66b8151456e2950c97
eloiji <eloisajocsoniting@gmail.com>
2025-09-30 09:09:30 +0800

    task: Exercise for React Testing Library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import CreateCustomHook from "./components/CreateCustomHook";
 import CustomRadioGroup from "./components/CustomRadioGroup";
+import FeedbackForm from "./components/FeedbackForm";
 import FetchingData from "./components/FetchingData";
 import ManagingStates from "./components/ManagingState";
 import RegistrationForm from "./components/RegistrationForm";
@@ -8,6 +9,10 @@ import ScrollerPositionWithRenderProps from "./components/ScrollerPositionWithRe
 import Root from "./components/ThemeSettings";
 
 function App() {
+  const handleSubmit = () => {
+    console.log("Form submitted!");
+  };
+
   return (
     <div className="App">
       <RegistrationForm />
@@ -17,6 +22,7 @@ function App() {
       <CreateCustomHook />
       <CustomRadioGroup />
       <ScrollerPositionWithRenderProps />
+      <FeedbackForm onSubmit={handleSubmit} />
     </div>
   );
 }
